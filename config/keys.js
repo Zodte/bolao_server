7093db7f2ee457fca1e4e39ad1760ac5ce92c18b
Zodte <pedro_viking@yahoo.com.br>
2018-05-04 10:17:37 +0000

    Environmental variables

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-  mongoURI:'mongodb://SareenAith:The%20Tigers7@ds235169.mlab.com:35169/bolao-dev',
-  googleClientID:'851532950798-k29s8ruq29peqkdh4vtrco08e1c35lir.apps.googleusercontent.com',
-  googleClientSecret:'LJ765OPSibcCO_O991uzK34b',
-  cookieKey: 'ASdkjasjgAkjashjkgafskljhasjglqAjma'
+if (process.env.NODE_ENV === 'production'){
+  //Production
+  module.exports = require('./prod');
+}else{
+  //development
+  module.exports = require('./dev');
 }
